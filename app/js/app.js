02f92de293e13a501e46aea96476f6533c54afb4
Abhi Agarwal <me@abhiagarwal.com>
2016-02-20 21:54:42 -0500

    Running through lint

--- app/js/app.js
+++ app/js/app.js
@@ -1,20 +1,20 @@
 $(function() {
     $.ajax({
-            beforeSend: function(xhrObj){
-                    xhrObj.setRequestHeader("Content-Type","application/vnd.api+json");
-                    xhrObj.setRequestHeader("Accept","application/vnd.api+json");
-            },
-            type: "GET",
-            url: "https://api.tnyu.org/v3/events?include=venue",
-            dataType: "json",
-            success: function(data){
-                var events = data.data;
-                var venues = data.included;
+        beforeSend: function(xhrObj) {
+            xhrObj.setRequestHeader("Content-Type", "application/vnd.api+json");
+            xhrObj.setRequestHeader("Accept", "application/vnd.api+json");
+        },
+        type: "GET",
+        url: "https://api.tnyu.org/v3/events?include=venue",
+        dataType: "json",
+        success: function(data) {
+            var events = data.data;
+            var venues = data.included;
 
-                var venueIdToVenue = {};
-                venues.forEach(function(venue) {
-                    venueIdToVenue[venue['id']] = venue;
-                });
-            }
+            var venueIdToVenue = {};
+            venues.forEach(function(venue) {
+                venueIdToVenue[venue['id']] = venue;
+            });
+        }
     });
 });
